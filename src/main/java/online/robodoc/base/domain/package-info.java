5d17229219b47acda8aa449c5777c869e5110fdf
start.vaadin.com <no-reply@vaadin.com>
2025-04-26 22:08:41 +0000

    Generated project

---
+++ src/main/java/online/robodoc/base/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable domain classes.
+ */
+@NullMarked
+package online.robodoc.base.domain;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/online/robodoc/base/ui/view/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable or cross-cutting view-related classes.
+ */
+@NullMarked
+package online.robodoc.base.ui.view;
+
+import org.jspecify.annotations.NullMarked;
