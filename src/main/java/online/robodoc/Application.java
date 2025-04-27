d6ad3435a0096bc9f5bdafa0c104eacacea25953
RoboDoc <133816684+theerikmaki@users.noreply.github.com>
2025-04-27 12:27:48 +0300

    Stopping spring framework security from hijacking the app

--- src/main/java/online/robodoc/Application.java
+++ src/main/java/online/robodoc/Application.java
@@ -5,13 +5,18 @@ import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.shared.communication.PushMode;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import java.time.Clock;
 
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class
+})
 @Push(PushMode.AUTOMATIC)
-@SpringBootApplication
 @Theme("default")
 public class Application implements AppShellConfigurator
 {
